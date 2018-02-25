41af59e63d9f94b9a49a952e035406f31209c0e3
Paul Shahid <pshahid90@gmail.com>
2018-02-25 15:42:21 -0500

    Updating dockerfile to be inline with config params
    - Adding some substance to helm chart's values.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cmd="${1:-run}"
 if [ "$cmd" == "test" ]; then
     pytest slackbucket/tests
 elif [ "$cmd" == "run" ]; then
-    python slackbucket/__main__.py
+    python bucket.py -c /usr/src/app/config.yaml
 fi
